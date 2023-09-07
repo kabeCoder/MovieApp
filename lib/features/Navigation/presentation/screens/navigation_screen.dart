09ac02306320b4cd79f01016b798c58b7b9cb561
Aldrin Mayor <aldrin.mayor@stratpoint.com>
2023-09-08 03:09:03 +0800

    style(Navigation): remove unused import

--- lib/features/Navigation/presentation/screens/navigation_screen.dart
+++ lib/features/Navigation/presentation/screens/navigation_screen.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_app/application/presentation/routes/app_router.dart';
 
 @RoutePage()
 class NavigationScreen extends StatelessWidget {
