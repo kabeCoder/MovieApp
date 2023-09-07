26da37381f5c1c874306999043680531037538fc
Aldrin Mayor <aldrin.mayor@stratpoint.com>
2023-09-07 23:32:48 +0800

    feat(Navigation): setup up auto route in bottom navigation screen

---
+++ lib/application/presentation/routes/app_router.gr.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_router.dart';
+
+abstract class _$AppRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    NavigationRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const NavigationScreen(),
+      );
+    }
+  };
+}
+
+/// generated route for
+/// [NavigationScreen]
+class NavigationRoute extends PageRouteInfo<void> {
+  const NavigationRoute({List<PageRouteInfo>? children})
+      : super(
+          NavigationRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'NavigationRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
---
+++ lib/features/Navigation/presentation/screens/navigation_screen.dart
@@ -0,0 +1,71 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class NavigationScreen extends StatelessWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsRouter.tabBar(
+      routes: [
+        // BooksTab(),
+        // ProfileTab(),
+        // SettingsTab(),
+      ],
+      builder: (context, child, controller) {
+        final tabsRouter = AutoTabsRouter.of(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(context.topRoute.name),
+            leading: const AutoLeadingButton(),
+            bottom: TabBar(
+              controller: controller,
+              tabs: const [
+                Tab(
+                  text: 'TV Shows',
+                  icon: Icon(Icons.abc),
+                ),
+                Tab(
+                  text: 'Movies',
+                  icon: Icon(Icons.abc),
+                ),
+                Tab(
+                  text: 'My List',
+                  icon: Icon(Icons.abc),
+                ),
+              ],
+            ),
+          ),
+          body: child,
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: tabsRouter.activeIndex,
+            onTap: tabsRouter.setActiveIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.movie),
+                label: 'Coming Soon',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.download),
+                label: 'Download',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.more),
+                label: 'More',
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
