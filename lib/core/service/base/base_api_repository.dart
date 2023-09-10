35d6d0423597ff0e9ccfc43df2b98f5325a0fd0b
Aldrin Mayor <aldrin.mayor@stratpoint.com>
2023-09-10 22:42:57 +0800

    feat(base-api-repository): created repository for api call and service call

---
+++ lib/core/service/base/base_api_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:movie_app/core/service/base/data/api_result.dart';
+
+class BaseApiRepository {
+  final String baseUrl;
+
+  BaseApiRepository({required this.baseUrl});
+
+  Future<ApiResult<T>> serviceCall<T>(Future<T> Function() callFunction) async {
+    try {
+      final result = await callFunction();
+      return ApiResult(data: result, error: null);
+    } catch (e) {
+      return ApiResult(data: null, error: e.toString());
+    }
+  }
+
+  Future<ApiResult<List<T>>> apiCall<T>(
+    Future<http.Response> Function() callFunction,
+    T Function(dynamic) fromJson,
+  ) async {
+    try {
+      final response = await callFunction();
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body);
+        final data = jsonData.map((dynamic item) => fromJson(item)).toList();
+        return ApiResult<List<T>>(data: data, error: null);
+      } else {
+        return ApiResult<List<T>>(data: [], error: 'HTTP Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      return ApiResult<List<T>>(data: [], error: 'Network Error: $e');
+    }
+  }
+}
